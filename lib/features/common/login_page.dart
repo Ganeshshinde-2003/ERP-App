cf4f1a8d85c6f5b18d1e36a4f6f025d1958a24f3
Ganesh Shinde <sindheganesh2004@gmail.com>
2024-02-18 21:54:00 +0530

    Created the snack bar

--- lib/features/common/login_page.dart
+++ lib/features/common/login_page.dart
@@ -39,7 +39,7 @@ class _LoginPageScreenState extends State<LoginPageScreen> {
         else
           {
             showSnackBar(
-              content: "Enter Username ID",
+              content: "Enter User ID",
               context: context,
             )
           }
